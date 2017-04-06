2bd929fd5d1c1d389d924595a8ad82289f908b4e
Mark Tareshawty <tarebyte@github.com>
2017-04-05 22:47:05 -0400

    Update styling to make it a little friendlier to use

--- app/components/HTTPHeaderEditor.js
+++ app/components/HTTPHeaderEditor.js
@@ -108,8 +108,10 @@ export default class HTTPHeaderEditor extends React.Component {
         <td>{header.key}</td>
         <td>{header.value.length > 40 ? header.value.substr(0, 40) + '...' : header.value}</td>
         <td>
-          <button onClick={() => this.editRow(i)}>Edit</button>
-          <button onClick={() => this.removeRow(i)}>&times;</button>
+          <div className="pure-button-group" style={{float: 'right'}} role={`edit-header-${header.key}-group`} aria-label={`Edit ${header.key}`}>
+            <button className="pure-button button-secondary" onClick={() => this.editRow(i)}>Edit</button>
+            <button className="pure-button button-error" onClick={() => this.removeRow(i)}>Delete</button>
+          </div>
         </td>
       </tr>
     )
@@ -119,26 +121,32 @@ export default class HTTPHeaderEditor extends React.Component {
     return (
       <tr key={`editing-row-${i}`}>
         <td>
-          <input
-            ref={(c) => (this[`editingRow${i}KeyInput`] = c)}
-            type="text"
-            placeholder="Header Key"
-            defaultValue={header.key}
-            style={{ width: '100%' }}
-          />
+          <form className="pure-form" style={{margin: '0px'}}>
+            <input
+              ref={(c) => (this[`editingRow${i}KeyInput`] = c)}
+              type="text"
+              placeholder="Header name"
+              defaultValue={header.key}
+              style={{ width: '100%' }}
+            />
+          </form>
         </td>
         <td>
-          <input
-            ref={(c) => (this[`editingRow${i}ValueInput`] = c)}
-            type="text"
-            placeholder="Header Value"
-            defaultValue={header.value}
-            style={{ width: '100%' }}
-          />
+          <form className="pure-form" style={{margin: '0px'}}>
+            <input
+              ref={(c) => (this[`editingRow${i}ValueInput`] = c)}
+              type="text"
+              placeholder="Header value"
+              defaultValue={header.value}
+              style={{ width: '100%' }}
+            />
+          </form>
         </td>
         <td>
-          <button onClick={() => this.completeEdit(i)}>&#x2713;</button>
-          <button onClick={() => this.cancelEdit(i)}>&times;</button>
+          <div className="pure-button-group" style={{float: 'right'}} role={`edit-header-${header.key}-group`} aria-label={`Edit ${header.key}`}>
+            <button className="pure-button button-success" onClick={() => this.completeEdit(i)}>Save</button>
+            <button className="pure-button button-error" onClick={() => this.cancelEdit(i)}>Cancel</button>
+          </div>
         </td>
       </tr>
     )
@@ -151,24 +159,30 @@ export default class HTTPHeaderEditor extends React.Component {
       addHeader = (
         <tr>
           <td>
-            <input
-              ref={(c) => (this.newKeyInput = c)}
-              type="text"
-              placeholder="Header Key"
-              style={{ width: '100%' }}
-            />
+            <form className="pure-form" style={{margin: '0px'}}>
+              <input
+                ref={(c) => (this.newKeyInput = c)}
+                type="text"
+                placeholder="Header name"
+                style={{ width: '100%' }}
+              />
+            </form>
           </td>
           <td>
-            <input
-              ref={(c) => (this.newValInput = c)}
-              type="text"
-              placeholder="Header Value"
-              style={{ width: '100%' }}
-            />
+            <form className="pure-form" style={{margin: '0px'}}>
+              <input
+                ref={(c) => (this.newValInput = c)}
+                type="text"
+                placeholder="Header value"
+                style={{ width: '100%' }}
+              />
+            </form>
           </td>
           <td>
-            <button onClick={this.completeAdd}>&#x2713;</button>
-            <button onClick={this.cancelAdd}>&times;</button>
+            <div className="pure-button-group" style={{float: 'right'}} role={`create-new-header-group`} aria-label={`Create new header`}>
+              <button className="pure-button button-success" onClick={this.completeAdd}>Save</button>
+              <button className="pure-button button-error" onClick={this.cancelAdd}>Cancel</button>
+            </div>
           </td>
         </tr>
       )
@@ -176,14 +190,14 @@ export default class HTTPHeaderEditor extends React.Component {
 
     return (
       <div className="headerEditor">
-        <h2>Edit HTTP Headers</h2>
+        <h2 style={{float: 'left'}}>Edit HTTP Headers</h2>
+        <a href="javascript:;" style={{float: 'right', margin: '1.24em'}} onClick={this.addHeader} className="pure-button pure-button-primary">+ Add Header</a>
         <div>
-          <a href="javascript:;" onClick={this.addHeader}>+ Add Header</a>
-          <table className="pure-table pure-table-striped" style={styles.table}>
+          <table className="pure-table pure-table-horizontal" style={styles.table}>
             <thead>
               <tr>
-                <th>Key</th>
-                <th>Value</th>
+                <th>Header name</th>
+                <th>Header value</th>
                 <th></th>
               </tr>
             </thead>
